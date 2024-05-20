551f65b60b8acf6d155d7758fbc779742852b038
ernest-tpximpact <ernest.man@tpximpact.com>
2024-05-20 13:57:51 +0100

    Add webserver config

--- dags/webserver_config.py
+++ dags/webserver_config.py
@@ -3,6 +3,12 @@ from flask_appbuilder.security.manager import AUTH_OAUTH
 
 # Enable OAuth authentication
 AUTH_TYPE = AUTH_OAUTH
+AUTH_ROLES_SYNC_AT_LOGIN = True
+AUTH_USER_REGISTRATION = True
+AUTH_ROLES_MAPPING = {
+    "Viewer": ["Viewer"],
+    "Admin": ["Admin"],
+}
 
 # Configure the OAuth provider
 OAUTH_PROVIDERS = [{
@@ -18,13 +24,6 @@ OAUTH_PROVIDERS = [{
         },
         'authorize_url': 'https://github.com/login/oauth/authorize',
         'access_token_url': 'https://github.com/login/oauth/access_token',
-        'authorize_params': {
-            'redirect_uri': os.getenv('AIRFLOW__CORE__GITHUBREDIRECTURI')
-        },
+         'request_token_url': None,
     }
 }]
-
-# Enable automatic user registration
-AUTH_USER_REGISTRATION = True
-AUTH_USER_REGISTRATION_ROLE = 'Viewer'  # Change as needed
-AUTH_ROLES_SYNC_AT_LOGIN = True
