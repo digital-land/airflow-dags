8ba5d7ae4cc3a3b978eb8eaf61b522898a860897
Christopher Johns <cjohns@scottlogic.com>
2024-07-11 09:28:16 +0100

    WIP

--- dags/dynamic_data_workflow_dag.py
+++ dags/dynamic_data_workflow_dag.py
@@ -5,6 +5,9 @@ from datetime import datetime
 from airflow.decorators import dag, task
 from airflow.providers.amazon.aws.operators.ecs import EcsRegisterTaskDefinitionOperator, EcsRunTaskOperator
 
+
+cluster_name = 'development-cluster'
+
 register_task = EcsRegisterTaskDefinitionOperator(
     task_id="hello",
     family="test",
@@ -38,40 +41,37 @@ configuration_file_path = os.path.join(my_dir, "config.json")
 with open(configuration_file_path) as file:
     configs = json.load(file)
 
-cluster_name = 'development-cluster'
 
-@task
-def hello():
-    hello_task = EcsRunTaskOperator(
-        task_id="hello",
-        cluster=cluster_name,
-        task_definition="hello",
-        launch_type="FARGATE",
-        overrides={
-            "containerOverrides": [
-                {
-                    "name": "hello",
-                    "command": ["echo", "hello", "world"],
-                },
-            ],
-        },
-        network_configuration={
-            "awsvpcConfiguration": {
-                #"subnets": test_context[SUBNETS_KEY],
-                #"securityGroups": test_context[SECURITY_GROUPS_KEY],
-                "assignPublicIp": "ENABLED",
-            },
-        },
-    )
-    return hello_task
 
 @dag(dag_id="test", start_date=datetime.today())
-def test_hello():
+def test_dag():
+
     @task
-    def hello_task():
-        return hello()
+    def hello():
+        hello_task = EcsRunTaskOperator(
+            task_id="hello",
+            cluster=cluster_name,
+            task_definition="hello",
+            launch_type="FARGATE",
+            overrides={
+                "containerOverrides": [
+                    {
+                        "name": "hello",
+                        "command": ["echo", "hello", "world"],
+                    },
+                ],
+            },
+            network_configuration={
+                "awsvpcConfiguration": {
+                    #"subnets": test_context[SUBNETS_KEY],
+                    #"securityGroups": test_context[SECURITY_GROUPS_KEY],
+                    "assignPublicIp": "ENABLED",
+                },
+            },
+        )
+        return hello_task
     
-    hello_task()
+    hello()
 
 """
 for collection,datasets in configs.items():
