0f030b4e116f27a10ff3c54701155b1ee1141244
Christopher Johns <cjohns@scottlogic.com>
2024-07-16 22:51:58 +0100

    WIP

--- dags/dynamic_data_workflow_dag.py
+++ dags/dynamic_data_workflow_dag.py
@@ -150,6 +150,7 @@ with DAG(
         #         "command": ["python", "-c", "import time; for i in range(30): print(i); time.sleep(10)"],
         #     },
         # ]},
+        network_mode="awsvpc",
         network_configuration={
             "awsvpcConfiguration": {
                 "subnets": ["subnet-05a0d548ea8d901ab", "subnet-07252405b5369afd3"],
