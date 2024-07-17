7b1382e29047a08140441f95cf55329d1d6526f7
Christopher Johns <cjohns@scottlogic.com>
2024-07-17 09:44:42 +0100

    WIP

--- dags/dynamic_data_workflow_dag.py
+++ dags/dynamic_data_workflow_dag.py
@@ -150,14 +150,13 @@ with DAG(
         #         "command": ["python", "-c", "import time; for i in range(30): print(i); time.sleep(10)"],
         #     },
         # ]},
-        network_configuration={
-            "networkMode": "awsvpc",
-            "awsvpcConfiguration": {
-                "subnets": ["subnet-05a0d548ea8d901ab", "subnet-07252405b5369afd3"],
-                "securityGroups": ["sg-0fe390dd951829c75"],
-                "assignPublicIp": "ENABLED",
-            }
-        },
+        #network_configuration={
+        #    "awsvpcConfiguration": {
+        #        "subnets": ["subnet-05a0d548ea8d901ab", "subnet-07252405b5369afd3"],
+        #        "securityGroups": ["sg-0fe390dd951829c75"],
+        #        "assignPublicIp": "ENABLED",
+        #    }
+        #},
         awslogs_group="airflow-development-mwaa-Task",
         awslogs_region="eu-west-1",
         awslogs_stream_prefix=f"ecs/test",
