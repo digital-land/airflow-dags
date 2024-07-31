8d2489cd65e94995265a7fd7c511935543917a31
eveleighoj <35256612+eveleighoj@users.noreply.github.com>
2024-07-31 20:55:45 +0100

    hardcode more values

--- dags/collection_generator.py
+++ dags/collection_generator.py
@@ -27,36 +27,6 @@ with open(configuration_file_path) as file:
     configs = json.load(file)
 
 
-# def run_collection_task():
-#     return EcsRegisterTaskDefinitionOperator(
-#         task_id="collection-task",
-#         family="collection",
-#         container_definitions=[
-#             {
-#                 "name": "collection-task",
-#                 "image": "public.ecr.aws/l6z6v3j6/development-mwaa-dataset-collection-task:publish-image",
-#                 "logConfiguration": {
-#                     "logDriver": "awslogs",
-#                     "options": {
-#                         "awslogs-create-group": "true",
-#                         "awslogs-group": log_group,
-#                         "awslogs-region": log_region,
-#                         "awslogs-stream-prefix": "collector",
-#                     },
-#                 },
-#             },
-#         ],
-#         register_task_kwargs={
-#             "cpu": "1024",
-#             "taskRoleArn": "arn:aws:iam::955696714113:role/development-mwaa-execution-role",
-#             "executionRoleArn": "arn:aws:iam::955696714113:role/development-mwaa-execution-role",
-#             "memory": "8192",
-#             "networkMode": "awsvpc",
-#             "requiresCompatibilities": ["FARGATE"],
-#         },
-#     )
-
-
 for collection, datasets in configs.items():
     dag_id = f"{collection}-collection"
 
@@ -67,55 +37,15 @@ for collection, datasets in configs.items():
         schedule=None,
     ) as dag:
         EcsRunTaskOperator(
-            task_id="collection",
+            task_id=f"{collection}-collection",
             dag=dag,
             execution_timeout=timedelta(minutes=10),
             cluster=cluster_name,
-            task_definition=collect_task_defn,
+            task_definition="development-mwaa-collection-task",
             launch_type="FARGATE",
             overrides={
                 "containerOverrides": [
                     {
-                        "name": "collection-task",
-                        "environment": [
-                            {"name": "COLLECTION_NAME", "value": collection}
-                        ],
-                    },
-                ]
-            },
-            network_configuration={
-                "awsvpcConfiguration": {
-                    "subnets": ["subnet-05a0d548ea8d901ab", "subnet-07252405b5369afd3"],
-                    "securityGroups": ["sg-0fe390dd951829c75"],
-                    "assignPublicIp": "ENABLED",
-                }
-            },
-            awslogs_group=log_group,
-            awslogs_region=log_region,
-            awslogs_stream_prefix="task",
-            awslogs_fetch_interval=timedelta(seconds=5)
-        )
-
-for collection, datasets in configs.items():
-    dag_id = f"{collection}-collection-old"
-
-    with DAG(
-        f"{collection}-collection-old",
-        default_args=default_args,
-        description=f"Collection task for the {collection} collection",
-        schedule=None,
-    ) as dag:
-        EcsRunTaskOperator(
-            task_id="collection-old",
-            dag=dag,
-            execution_timeout=timedelta(minutes=10),
-            cluster=cluster_name,
-            task_definition="collection",
-            launch_type="FARGATE",
-            overrides={
-                "containerOverrides": [
-                    {
-                        "name": "collection-task",
                         "environment": [
                             {"name": "COLLECTION_NAME", "value": collection}
                         ],
@@ -130,110 +60,7 @@ for collection, datasets in configs.items():
                 }
             },
             awslogs_group="airflow-development-mwaa-Task",
-            awslogs_region="eu-west-1",
-            awslogs_stream_prefix="collector",
+            awslogs_region="eu-west-2",
+            awslogs_stream_prefix="task",
             awslogs_fetch_interval=timedelta(seconds=5)
         )
-
-# with DAG(
-#     "EC2-Test",
-#     default_args=default_args,
-#     description="A test DAG to try out functionality",
-#     schedule=None,
-# ) as dag:
-#     EcsRunTaskOperator(
-#         task_id="ec2-test",
-#         dag=dag,
-#         execution_timeout=timedelta(minutes=5),
-#         # retries=3,
-#         # aws_conn_id="aws_default",
-#         cluster=cluster_name,
-#         task_definition="airflow-ecs-operator-test",  # register_task.output,#",
-#         launch_type="EC2",
-#         overrides={},
-#         # overrides={"containerOverrides": [
-#         #     {
-#         #         "name": "test",
-#         #         "command": ["python", "-c", "import time; for i in range(30): print(i); time.sleep(1)"],
-#         #     },
-#         # ]},
-#         network_configuration={
-#             "awsvpcConfiguration": {
-#                 "subnets": ["subnet-05a0d548ea8d901ab", "subnet-07252405b5369afd3"],
-#                 "securityGroups": ["sg-0fe390dd951829c75"],
-#                 # "assignPublicIp": "ENABLED",
-#             }
-#         },
-#         awslogs_group="airflow-development-mwaa-Task",
-#         awslogs_region="eu-west-1",
-#         # awslogs_stream_prefix=f"ec2/test",
-#         # awslogs_fetch_interval=timedelta(seconds=5)
-#     )
-
-
-# with DAG(
-#     "Fargate-Test",
-#     default_args=default_args,
-#     description="A test DAG to try out functionality",
-#     schedule=None,
-# ) as dag:    
-#     test_task = EcsRegisterTaskDefinitionOperator(
-#         task_id="register-test-task",
-#         family="test",
-#         container_definitions=[
-#             {
-#                 "name": "hello",
-#                 "image": "ubuntu",
-#                 "workingDirectory": "/usr/bin",
-#                 "entryPoint": ["sh", "-c"],
-#                 "command": ["ls"],
-#                 "logConfiguration": {
-#                     "logDriver": "awslogs",
-#                     "options": {
-#                         "awslogs-create-group": "true",
-#                         "awslogs-group": log_group,
-#                         "awslogs-region": log_region,
-#                         "awslogs-stream-prefix": "ecs",
-#                     },
-#                 },
-#             },
-#         ],
-#         register_task_kwargs={
-#             "cpu": "256",
-#             "taskRoleArn": "arn:aws:iam::955696714113:role/development-mwaa-execution-role",
-#             "executionRoleArn": "arn:aws:iam::955696714113:role/development-mwaa-execution-role",
-#             "memory": "512",
-#             "networkMode": "awsvpc",
-#             "requiresCompatibilities": ["FARGATE"],
-#         },
-#     )
-
-#     EcsRunTaskOperator(
-#         task_id="run-test-task",
-#         dag=dag,
-#         execution_timeout=timedelta(minutes=5),
-#         # retries=3,
-#         # aws_conn_id="aws_default",
-#         cluster=cluster_name,
-#         task_definition=test_task.output,
-#         launch_type="FARGATE",
-#         overrides={
-#             "containerOverrides": [
-#                 {
-#                     "name": "hello",
-#                     "command": ["uname"],
-#                 },
-#             ]
-#         },
-#         network_configuration={
-#             "awsvpcConfiguration": {
-#                 "subnets": ["subnet-05a0d548ea8d901ab", "subnet-07252405b5369afd3"],
-#                 "securityGroups": ["sg-0fe390dd951829c75"],
-#                 "assignPublicIp": "ENABLED",
-#             }
-#         },
-#         awslogs_group=log_group,
-#         awslogs_region=log_region,
-#         awslogs_stream_prefix="ecs/hello",
-#         awslogs_fetch_interval=timedelta(seconds=5),
-#     )
