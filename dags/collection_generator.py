b11fa980d162cffe5a28a248782d53f6baee171f
eveleighoj <35256612+eveleighoj@users.noreply.github.com>
2024-07-31 19:50:00 +0100

    update prefix

--- dags/collection_generator.py
+++ dags/collection_generator.py
@@ -92,7 +92,7 @@ for collection, datasets in configs.items():
             },
             awslogs_group=log_group,
             awslogs_region=log_region,
-            awslogs_stream_prefix="collector/collection-task",
+            awslogs_stream_prefix="task",
             awslogs_fetch_interval=timedelta(seconds=5)
         )
 
