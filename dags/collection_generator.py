ddd13684b6a2ce9f782465b1a657cb40c2e6e89e
eveleighoj <35256612+eveleighoj@users.noreply.github.com>
2024-08-16 13:07:39 +0100

    moer attempts

--- dags/collection_generator.py
+++ dags/collection_generator.py
@@ -64,7 +64,7 @@ for collection, datasets in configs.items():
         EcsRunTaskOperator(
             task_id=f"{collection}-collection",
             dag=dag,
-            execution_timeout=timedelta(minutes= params['timeout']),
+            execution_timeout=timedelta(minutes= int('{{ params.timeout  | int }}')),
             cluster=cluster_name,
             task_definition="development-mwaa-collection-task",
             launch_type="FARGATE",
