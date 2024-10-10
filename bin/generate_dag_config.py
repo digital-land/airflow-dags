f478638a5c7bbdb75bf3834052c2453c5ba057eb
ernest-tpximpact <ernest.man@tpximpact.com>
2024-10-10 16:04:10 +0100

    test schedule

--- bin/generate_dag_config.py
+++ bin/generate_dag_config.py
@@ -19,7 +19,7 @@ collection_config = Environments(
             'central-activities-zone'
         ],
         # schedule='0 10 * * *'  # Daily at 11 AM
-        schedule='0 15 * * *'
+        schedule='15 15 * * *'
     ),
     staging=CollectionConfig(
         selection=CollectionSelection.all
