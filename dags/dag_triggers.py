63c1e86f7a7efc0eb830cea34d80f9c5bb6f688c
Chris Cundill <chrisc@diligentsoft.co>
2024-10-22 09:28:47 +0100

    Added another DAG of DAGs to enable running all collection DAGs manually

--- dags/dag_triggers.py
+++ dags/dag_triggers.py
@@ -6,9 +6,6 @@ collection_generator.py
 from airflow import DAG
 from airflow.operators.trigger_dagrun import TriggerDagRunOperator
 
-import os
-import json
-
 from datetime import datetime, timedelta
 
 from utils import get_config, load_specification_datasets
@@ -28,8 +25,8 @@ def collection_selected(collection_name, configuration):
 
 
 with DAG(
-        dag_id="trigger-collection-dags",
-        description=f"A master DAG which runs all the processing we need each need to process all datasets and packages",
+        dag_id="trigger-collection-dags-scheduled",
+        description=f"A master DAG which runs all selected collection DAGs on a scheduled basis",
         schedule=dag_schedule,
         start_date=datetime(2024, 1, 1),
         catchup=False,
@@ -55,3 +52,29 @@ with DAG(
                 )
                 if organisation_collection_selected:
                     run_org_dag >> collection_dag
+
+
+with DAG(
+        dag_id="trigger-collection-dags-manual",
+        description=f"A master DAG which runs all collection DAGs on a manual basis",
+        catchup=False,
+        max_active_tasks=dag_max_active_tasks,
+        is_paused_upon_creation=False
+):
+
+    run_org_dag = TriggerDagRunOperator(
+        task_id='trigger-organisation-collection-dag',
+        trigger_dag_id=f'organisation-collection'
+    )
+
+    collections = load_specification_datasets()
+
+    for collection, datasets in collections.items():
+        if collection not in ['organisation']:
+
+            collection_dag = TriggerDagRunOperator(
+                task_id=f'trigger-{collection}-collection-dag',
+                trigger_dag_id=f'{collection}-collection'
+            )
+
+            run_org_dag >> collection_dag
