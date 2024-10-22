f6420c43eae4c48a641ea51ceb2a6afa302a126c
Chris Cundill <chrisc@diligentsoft.co>
2024-10-22 09:37:21 +0100

    Pause manual DAG of DAGs upon creation

--- dags/dag_triggers.py
+++ dags/dag_triggers.py
@@ -59,7 +59,7 @@ with DAG(
         description=f"A master DAG which runs all collection DAGs on a manual basis",
         catchup=False,
         max_active_tasks=dag_max_active_tasks,
-        is_paused_upon_creation=False
+        is_paused_upon_creation=True
 ):
 
     run_org_dag = TriggerDagRunOperator(
