aa09595ebbf7d11db7e999a7ba3d1005560af636
Chris Cundill <chrisc@diligentsoft.co>
2024-10-22 08:24:12 +0100

    Removed restriction on title-boundary collection since collection DAGS included within  scheduled dag are now defined in config

--- dags/dag_triggers.py
+++ dags/dag_triggers.py
@@ -44,7 +44,7 @@ with DAG(
     collections = load_specification_datasets()
 
     for collection, datasets in collections.items():
-        if collection not in ['organisation', 'title-boundary']:
+        if collection not in ['organisation']:
             if collection_selected(collection, config):
                 collection_dag = TriggerDagRunOperator(
                     task_id=f'trigger-{collection}-collection-dag',
