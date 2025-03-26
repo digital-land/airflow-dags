229137af93750ba79d814dab531f4bb517fcb22c
Chris Cundill <chrisc@diligentsoft.co>
2025-03-26 13:04:57 +0000

    Now checking if pg_restore can be run within BashOperator

--- dags/scripts/postgres_restore.sh
+++ dags/scripts/postgres_restore.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 echo "Hello Airflow bash!"
 curl https://github.com/digital-land/planning-data-design/raw/refs/heads/main/data/latest_backup.dump > postgres.dump
+pg_restore --dbname=TBC --data-only postgres.dump
