403a8500274df37c2fa361c5e5685d1d6dc0792c
eveleighoj <35256612+eveleighoj@users.noreply.github.com>
2025-07-03 16:32:49 +0100

    include sqlite_ingestion (#36)
    * add first dag
    
    * correct containe related things
    
    * corect memory and cpu defaults
    
    * incorect key for xcom pull
    
    * correct xcom pulls for logs
    
    * let any dataset be selected
    
    * first attempt
    
    * add missing bracket
    
    * correct  task name
    
    * correct container name
    
    * make final tweaks and add loading to digital-land-builder

---
+++ dags/manual_postgres_loader.py
@@ -0,0 +1,140 @@
+"""
+Module containing DAG to load a dataset into the Postgis database in RDS
+"""
+import os
+import json
+import boto3
+
+from datetime import datetime, timedelta
+from airflow import DAG
+from airflow.decorators import dag
+from airflow.providers.amazon.aws.operators.ecs import (
+    EcsRegisterTaskDefinitionOperator,
+    EcsRunTaskOperator,
+)
+
+from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
+from airflow.models.param import Param
+from airflow.models import Variable
+from airflow.models.connection import Connection
+from airflow.providers.slack.notifications.slack import send_slack_notification
+
+from utils import dag_default_args,get_config, load_specification_datasets,get_dataset_collection, get_task_log_config,get_datasets
+
+config = get_config()
+collections_dict = load_specification_datasets()
+
+# define some inputs
+collection_datasets = get_datasets(collections_dict)
+# append digital-land so data contained in there can be loaded
+collection_datasets.append('digital-land')
+
+ecs_cluster = f"{config['env']}-cluster"
+sqlite_injection_task_name = f"{config['env']}-sqlite-ingestion-task"
+container_name = f"{config['env']}-sqlite-ingestion"
+
+with DAG(
+    "manual-postgres-loader",
+    default_args=dag_default_args,
+    description=f"A DAG which is not scheduled and can be mran manually to upload a dataset to the Postgis database in RDS without the need to run other processing",
+    schedule=None,
+    catchup=False,
+    params={
+        "cpu": Param(default=1024, type="integer"),
+        "memory": Param(default=4096, type="integer"),
+        "dataset": Param(type="string",enum=collection_datasets),
+    },
+    render_template_as_native_obj=True,
+    is_paused_upon_creation=False,
+) as dag:
+    
+    def configure_dag(**kwargs):
+        # get params from DAG params 
+        params = kwargs['params']
+
+        # based on the dataset provided need to create an ARN
+        # this isn't actually used to download the dataset but the bucket name 
+        # and key are
+        collection_dataset_bucket_name = kwargs['conf'].get(section='custom', key='collection_dataset_bucket_name')
+        dataset = str(params.get('dataset'))
+        if dataset == 'digital-land':
+            collection = 'digital-land-builder'
+        else:
+            collection = get_dataset_collection(collections_dict,dataset)
+
+        s3_object_arn = f"arn:aws:s3:::{collection_dataset_bucket_name}/{collection}-collection/dataset/{dataset}.sqlite3"
+
+        # getmemory and cpu from params
+        memory = int(params.get('memory'))
+        cpu = int(params.get('cpu'))
+        ti = kwargs['ti']
+        ti.xcom_push(key='env', value=config['env'])
+        ti.xcom_push(key='memory', value=memory)
+        ti.xcom_push(key='cpu', value=cpu)
+        ti.xcom_push(key='dataset', value=dataset)
+        ti.xcom_push(key='collection', value=collection)
+        ti.xcom_push(key='s3-object-arn', value=s3_object_arn)
+
+        # add vpc details
+        aws_vpc_config = {
+            "subnets": kwargs['conf'].get(section='custom', key='ecs_task_subnets').split(","),
+            "securityGroups": kwargs['conf'].get(section='custom', key='ecs_task_security_groups').split(","),
+            "assignPublicIp": "ENABLED",
+        }
+        ti.xcom_push(key='aws_vpc_config', value=aws_vpc_config)
+
+        # need to get log group information
+        ecs_client = boto3.client('ecs')
+        sqlite_injection_task_log_config = get_task_log_config(ecs_client, sqlite_injection_task_name)
+        sqlite_injection_task_log_config_options = sqlite_injection_task_log_config['options']
+        sqlite_injection_task_log_group = str(sqlite_injection_task_log_config_options.get('awslogs-group'))
+        sqlite_injection_task_log_stream_prefix = (str(sqlite_injection_task_log_config_options.get('awslogs-stream-prefix'))
+                                             + f'/{container_name}')
+        sqlite_injection_task_log_region = str(sqlite_injection_task_log_config_options.get('awslogs-region'))
+
+        # all the info we need to feed into the ECS task
+        ti.xcom_push(key='sqlite-injection-task-log-group', value=sqlite_injection_task_log_group)
+        ti.xcom_push(key='sqlite-injection-task-log-stream-prefix', value=sqlite_injection_task_log_stream_prefix)
+        ti.xcom_push(key='sqlite-injection-task-log-region', value=sqlite_injection_task_log_region)
+        ti.xcom_push(key='collection-dataset-bucket-name', value=collection_dataset_bucket_name)
+
+    configure_dag_task = PythonOperator(
+        task_id="configure-dag",
+        python_callable=configure_dag,
+        dag=dag,
+    )
+
+    load_dataset_ecs_task = EcsRunTaskOperator(
+        task_id=f"manual-load-dataset",
+        dag=dag,
+        execution_timeout=timedelta(minutes=1800),
+        cluster=ecs_cluster,
+        task_definition=sqlite_injection_task_name,
+        launch_type="FARGATE",
+        overrides={
+            "containerOverrides": [
+                {
+                    "name": container_name,
+                    'cpu': '{{ task_instance.xcom_pull(task_ids="configure-dag", key="cpu") | int }}', 
+                    'memory': '{{ task_instance.xcom_pull(task_ids="configure-dag", key="memory") | int }}', 
+                    "environment": [
+                        {"name": "ENVIRONMENT", "value": "'{{ task_instance.xcom_pull(task_ids=\"configure-dag\", key=\"env\") | string }}'"},
+                        {
+                            "name": "S3_OBJECT_ARN",
+                            "value": "'{{ task_instance.xcom_pull(task_ids=\"configure-dag\", key=\"s3-object-arn\") | string }}'"
+                        },
+                    ],
+                },
+            ]
+        },
+        network_configuration={
+            "awsvpcConfiguration": '{{ task_instance.xcom_pull(task_ids="configure-dag", key="aws_vpc_config") }}'
+        },
+        awslogs_group='{{ task_instance.xcom_pull(task_ids="configure-dag", key="sqlite-injection-task-log-group") }}',
+        awslogs_region='{{ task_instance.xcom_pull(task_ids="configure-dag", key="sqlite-injection-task-log-region") }}',
+        awslogs_stream_prefix='{{ task_instance.xcom_pull(task_ids="configure-dag", key="sqlite-injection-task-log-stream-prefix") }}',
+        awslogs_fetch_interval=timedelta(seconds=1)
+    )
+
+    configure_dag_task >> load_dataset_ecs_task
--- dags/utils.py
+++ dags/utils.py
@@ -27,6 +27,7 @@ def get_config(path=None):
     return config
 
 
+# TBD no idea why this isn't in the config rather than directly loading stuff from the spec
 def load_specification_datasets():
     with tempfile.TemporaryDirectory() as tmpdir:
 
@@ -65,6 +66,36 @@ def get_task_log_config(ecs_client, task_definition_family):
     
     return log_config
 
+def push_vpc_config(ti,conf):
+    """
+    Pushes the VPC configuration details to XComs for use in other tasks.
+    """
+    aws_vpc_config = {
+        "subnets": conf.get(section='custom', key='ecs_task_subnets').split(","),
+        "securityGroups": conf.get(section='custom', key='ecs_task_security_groups').split(","),
+        "assignPublicIp": "ENABLED",
+    }
+    
+    ti.xcom_push(key='aws_vpc_config', value=aws_vpc_config)
+
+def push_log_variables(ti,task_definition_name,container_name,prefix):
+    """
+    Given an ECS task definition name and container name can push
+    the log configuration details to XComs for use in other tasks.
+    """
+    # get ecs-task logging configuration
+    ecs_client = boto3.client('ecs')
+    task_log_config = get_task_log_config(ecs_client, task_definition_name)
+    task_log_config_options = task_log_config['options']
+    task_log_group = str(task_log_config_options.get('awslogs-group'))
+    # add container name to prefix
+    task_log_stream_prefix = (str(task_log_config_options.get('awslogs-stream-prefix'))
+                                            + f'/{container_name}')
+    task_log_region = str(task_log_config_options.get('awslogs-region'))
+
+    ti.xcom_push(key=f'{prefix}-log-group', value=task_log_group)
+    ti.xcom_push(key=f'{prefix}-log-stream-prefix', value=task_log_stream_prefix)
+    ti.xcom_push(key=f'{prefix}-log-region', value=task_log_region)
 
 def setup_configure_dag_callable(config, task_definition_name):
     def configure_dag(**kwargs):
@@ -75,13 +106,13 @@ def setup_configure_dag_callable(config, task_definition_name):
         - read in env variables
         - access options defined in the task definitions
         """
-        aws_vpc_config = {
-            "subnets": kwargs['conf'].get(section='custom', key='ecs_task_subnets').split(","),
-            "securityGroups": kwargs['conf'].get(section='custom', key='ecs_task_security_groups').split(","),
-            "assignPublicIp": "ENABLED",
-        }
 
-        # retrieve and process parameters
+        ti = kwargs['ti']
+
+        # add env from config
+        ti.xcom_push(key='env', value=config['env'])
+
+        # add DAG parameters
         params = kwargs['params']
 
         memory = int(params.get('memory'))
@@ -91,30 +122,44 @@ def setup_configure_dag_callable(config, task_definition_name):
         incremental_loading_override = bool(kwargs['params'].get('incremental-loading-override'))
         regenerate_log_override = bool(kwargs['params'].get('regenerate-log-override'))
 
-        # get ecs-task logging configuration
-        ecs_client = boto3.client('ecs')
-        collection_task_log_config = get_task_log_config(ecs_client, task_definition_name)
-        collection_task_log_config_options = collection_task_log_config['options']
-        collection_task_log_group = str(collection_task_log_config_options.get('awslogs-group'))
-        # add container name to prefix
-        collection_task_log_stream_prefix = (str(collection_task_log_config_options.get('awslogs-stream-prefix'))
-                                             + f'/{task_definition_name}')
-        collection_task_log_region = str(collection_task_log_config_options.get('awslogs-region'))
-        collection_dataset_bucket_name = kwargs['conf'].get(section='custom', key='collection_dataset_bucket_name')
-
         # Push values to XCom
-        ti = kwargs['ti']
-        ti.xcom_push(key='env', value=config['env'])
-        ti.xcom_push(key='aws_vpc_config', value=aws_vpc_config)
         ti.xcom_push(key='memory', value=memory)
         ti.xcom_push(key='cpu', value=cpu)
         ti.xcom_push(key='transformed-jobs', value=transformed_jobs)
         ti.xcom_push(key='dataset-jobs', value=dataset_jobs)
-        ti.xcom_push(key='collection-task-log-group', value=collection_task_log_group)
-        ti.xcom_push(key='collection-task-log-stream-prefix', value=collection_task_log_stream_prefix)
-        ti.xcom_push(key='collection-task-log-region', value=collection_task_log_region)
-        ti.xcom_push(key='collection-dataset-bucket-name', value=collection_dataset_bucket_name)
         ti.xcom_push(key='incremental-loading-override', value=incremental_loading_override)
         ti.xcom_push(key='regenerate-log-override', value=regenerate_log_override)
 
+
+        # add collection_data bucket # add collection bucket name
+        collection_dataset_bucket_name = kwargs['conf'].get(section='custom', key='collection_dataset_bucket_name')
+        ti.xcom_push(key='collection-dataset-bucket-name', value=collection_dataset_bucket_name)
+
+        # push collection-task log variables
+        push_log_variables(ti,task_definition_name=task_definition_name,container_name=task_definition_name,prefix='collection-task')
+        # push aws vpc config
+        push_vpc_config(ti, kwargs['conf'])
+        
+
     return configure_dag
+
+def get_dataset_collection(collections_dict, dataset):
+    """
+    Given a dictionary of collections and datasets, return the collection name for a given dataset.
+    If the dataset is not found, return None.
+    """
+    for collection, datasets in collections_dict.items():
+        if dataset in datasets:
+            return collection
+    return None
+
+def get_datasets(collections_dict):
+    """
+    Given a dictionary of collections and datasets, return the list of datasets for a given collection.
+    If the collection is not found, return an empty list.
+    """
+    all_datasets = []
+    for collection, datasets in collections_dict.items():
+            all_datasets.extend(datasets)
+
+    return all_datasets
