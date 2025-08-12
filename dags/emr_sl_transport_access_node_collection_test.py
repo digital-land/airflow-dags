3a8f755c449af273cca205f0eb1590d71d04edaa
lakshmi <LakshmiVenkataRatnaKumari.Kovvuri@cognizant.com>
2025-08-12 16:14:38 +0100

    renamed and changed the DAG id #874 renamed:    dags/emr_sl_transport_access_node_collection_1.py -> dags/emr_sl_transport_access_node_collection_test.py

--- dags/emr_sl_transport_access_node_collection_test.py
+++ dags/emr_sl_transport_access_node_collection_test.py
@@ -69,7 +69,7 @@ def wait_for_emr_job_completion(**context):
 
 # Define the DAG to run EMR Serverless PySpark job
 with DAG(
-    dag_id='emr_sl_transport_access_node_collection',
+    dag_id='emr_sl_transport_access_node_collection_test',
     default_args=default_args,
     description='Run transport-access-node PySpark job on EMR Serverless',
     schedule_interval=None,  # Manual trigger only
